34cc7ac892f44056aa8c992ca7666e3c65dcb8fc
elektroid <elektroid@nobodix.org>
2014-07-07 00:40:57 +0200

    ugly tests

---
+++ server/testParser.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Purpose:
+#
+# Author:      Elektroid
+#
+#-------------------------------------------------------------------------------
+
+import sys
+sys.path.append("..")
+import protocol
+import json
+
+
+def main():
+    prot=protocol.Protocol()
+    json_data = open(sys.argv[1])
+    data=json_data.read()
+    print "treat :"+data
+    print prot.treatMessage(data)
+
+
+
+if __name__ == '__main__':
+    main()
